0bf6a54aa1bd263a578cd6781be17caa5014ed4c
tmknom <tmk.nom@gmail.com>
2021-10-18 13:17:20 +0900

    install_homebrew.shをdotfilesリポジトリからコピー
    コピー元はコミットで削除したバージョン
    https://github.com/tmknom/dotfiles/commit/0a33187fc038f54a0aaa7108678b94da59aae7cb

---
+++ scripts/install_homebrew.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#
+# Homebrew自体のインストール
+
+set -eu -o pipefail
+
+# 無限に入力を待たないよう注意喚起
+message() {
+  cat <<-'EOL'
+Start install homebrew...
+
+1. Input password
+2. Press return key
+3. Wait about ten minutes
+
+EOL
+} >&2
+
+# brewコマンドが見つからなかったときだけHomebrewをインストール
+# https://brew.sh/index_ja
+if ! command -v brew >/dev/null 2>&1; then
+  message
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+printf "\n\033[32mInstalled Homebrew.\033[0m\n"
