32a20249246e15a478a438ab14f447ca0d4497ff
tmknom <tmk.nom@gmail.com>
2021-10-18 13:19:45 +0900

    install_asdf.shをdotfilesリポジトリからコピー
    コピー元はコミットで削除したバージョン
    https://github.com/tmknom/dotfiles/commit/cdeb47642cc633bbaf458a988feea7882b1b68de

---
+++ scripts/install_asdf.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+#
+# asdfによるCLIツールのインストール
+
+set -eu -o pipefail
+
+# Homebrewがインストールされているかチェック
+if ! command -v brew >/dev/null 2>&1; then
+  brew_message_command="make install-homebrew"
+  printf "Not found brew, run following command:\n\n  \033[31m%s\033[0m\n" "${brew_message_command}" >&2
+  exit 1
+fi
+
+# asdfがインストールされているかチェック
+if ! command -v asdf >/dev/null 2>&1; then
+  asdf_message_command="make install-cli"
+  printf "Not found asdf, run following command:\n\n  \033[31m%s\033[0m\n" "${asdf_message_command}" >&2
+  exit 1
+fi
+
+# 念のためasdfを最新化
+echo "Run brew upgrade asdf..."
+brew upgrade asdf
+
+# プラグインを最新化
+asdf plugin update --all
+
+# Terraformのインストール
+echo "Run asdf install terraform..."
+if ! command -v terraform >/dev/null 2>&1; then
+  asdf plugin add terraform https://github.com/Banno/asdf-hashicorp.git
+fi
+asdf install terraform latest
+asdf global terraform latest
+
+# AWS CLIのインストール
+echo "Run asdf install awscli..."
+if ! command -v aws >/dev/null 2>&1; then
+  asdf plugin add awscli https://github.com/MetricMike/asdf-awscli.git
+fi
+asdf install awscli latest
+asdf global awscli latest
+
+printf "\n\033[32mInstalled CLI tools by asdf.\033[0m\n"
